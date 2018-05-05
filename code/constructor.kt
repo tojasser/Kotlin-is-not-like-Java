1d76b4615c4ab826a5761295f097d407a625938c
abdulazizalawshan <abdulaziz.aloshin@gmail.com>
2018-05-05 13:12:50 +0300

    constructor.kt fix issues

--- code/constructor.kt
+++ code/constructor.kt
@@ -1,8 +1,7 @@
 class Student private constructor(firstName: String)
-// If the primary constructor does not have any annotations or visibility modifiers, the constructor keyword can be omitted:
+// If the primary constructor does not have any annotations or visibility modifiers, the constructor keyword can be omitted.
 
-////default values for constructor arguments
-////val ali = Student(20,12,"ali")
+
 class Student(var score:Int, var id: Int,name: String){
     init {
         print("Hi, my name is $name, ")
@@ -36,7 +35,7 @@ class Student (var id: Int,name: String){
 // val ali = Student() if you send nothing default value is 20 and ali
 // or val ali = Student(12,"Aziz")
 // val Aziz = Student(name = "Aziz") //You can explicitly specify names for some or all constructor arguments.
-class Student (var id: Int = 20,name: String)
+class Student (var id: Int = 20,name: String = "ali")
 
 // If a non-abstract class does not declare any constructors (primary or secondary),
 // it will have a generated primary constructor with no arguments
