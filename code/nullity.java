0821a2c187564c32fac5767812b249d0dda5c570
tojasser <37698683+tojasser@users.noreply.github.com>
2018-07-16 09:08:55 +0300

    Update nullity.java

--- code/nullity.java
+++ code/nullity.java
@@ -1,15 +1,1 @@
-String x = null;
-String y = "Value";
-// check nullity
-if (x != null){
-    y = y + " " + x;
-}else {
-    x = y;
-}
 
-x = null;
-// check type
-if (x instanceof String) {
-    String z = x;
-    System.out.print(z.length());
-}
